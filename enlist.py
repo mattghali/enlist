b09c9d6d0e377595e4f5c98bcc48ce4c1c1f5590
Matt Ghali <ghalim@justin.tv>
2017-07-23 17:25:13 -0700

    efficiency fixes, watch ratelimits

--- enlist.py
+++ enlist.py
@@ -29,26 +29,49 @@ class Connection(object):
                           access_token_key=self.access_token_key,
                           access_token_secret=self.access_token_secret)
 
+        self.api.InitializeRateLimit()
         self.me = self.api.GetUser(screen_name=self.screen_name)
 
 
-    def addFollowers(self, user, slug, next=-1, count=200):
-        if self.args.verbose: sys.stderr.write("listing followers of %s\n" % user.screen_name)
+    def addFollowers(self, user, slug, next=-1, count=200, bulk=False):
+        if self.api.rate_limit.resources['followers']['/followers/list'].get('remaining', 15) == 0:
+            if self.args.verbose: sys.stderr.write('out of rate limit credits, bailing')
+            return next
+
         (next, prev, follow) = self.api.GetFollowersPaged(cursor=next,
                                                         count=count,
                                                         skip_status=True,
                                                         user_id=user.id,
                                                         include_user_entities=False)
-        for f in follow:
-            if self.args.verbose: sys.stderr.write("adding %s\n" % f.screen_name)
-            try:
-                self.api.CreateListsMember(slug=slug, user_id=f.id,
-                                            owner_screen_name=self.screen_name)
-            except twitter.error.TwitterError, e:
-                pass # already listed or blocked
+        if bulk:
+            # adds 'count' at once, unless something fails
+            users = [ u.id for u in follow ]
+            if self.args.verbose: sys.stderr.write("adding %s users\n" % len(users))
+            self.addToList(slug, users)
+
+        else:
+            # adds one precious chud at a time
+            for f in follow:
+                if self.args.verbose: sys.stderr.write("adding %s\n" % f.screen_name)
+                self.addToList(slug, f.id)
             
         if next:
-            self.addFollowers(user, slug, next=next, count=count)
+            self.addFollowers(user, slug, next=next, count=count, bulk=bulk)
+
+        else:
+            # finally, block megachud himself (which removes him from megachud list)
+            if self.args.verbose: sys.stderr.write("finally adding %s\n" % user.screen_name)
+            self.addToList(slug, user)
+
+        return -1
+
+
+    def addToList(self, slug, user):
+        try:
+            self.api.CreateListsMember(slug=slug, user_id=user,
+                                       owner_screen_name=self.screen_name)
+        except twitter.error.TwitterError, e:
+            if self.args.verbose: sys.stderr.write("error: %s\n" % e)
 
 
     def block(self, user):
@@ -62,6 +85,12 @@ class Connection(object):
             if self.args.verbose: sys.stderr.write("exception: %s\n" % e)
 
 
+    def limits(self):
+        resources = self.api.rate_limit.resources
+        for res in resources:
+            for ep in  resources[res]:
+                if resources[res][ep]['remaining'] < resources[res][ep]['limit']:
+                         return  "%s: %s" % (ep, resources[res][ep])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
@@ -81,17 +110,21 @@ if __name__ == '__main__':
             if args.verbose: sys.stderr.write("created list: %s\n" % i)
 
     # main loop
+    next = -1
     while True:
         megachuds = conn.api.GetListMembers(slug=args.megachuds_list, owner_screen_name=conn.screen_name)
         for megachud in megachuds:
-            if args.verbose: sys.stderr.write("adding megachud %s\n" % megachud.screen_name)
-            conn.addFollowers(megachud, args.chuds_list)
-            conn.block(megachud)
-
+            if args.verbose:
+                if next < 0:
+                    sys.stderr.write("adding megachud %s\n" % megachud.screen_name)
+                else:
+                    sys.stderr.write("continuing megachud %s\n" % megachud.screen_name)
+            next = conn.addFollowers(megachud, args.chuds_list, next=next, count=100, bulk=True)
 
         chuds = conn.api.GetListMembers(slug=args.chuds_list, owner_screen_name=conn.screen_name)
         for chud in chuds:
             conn.block(chud)
 
         if args.verbose: sys.stderr.write('bottom of the loop\n')
+        if args.verbose: sys.stderr.write("%s\n" % conn.limits())
         time.sleep(args.sleep)
